24d31c8b65563f648043fab3eec57e6a913fb8db
Rodionov Pavel <pavel.rodionov.developer@gmail.com>
2022-08-04 07:41:18 +0800

    получение адресов в консольной комманде

--- src/Command/NotificationCommand.php
+++ src/Command/NotificationCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Command;
 
+use App\Entity\Email;
 use App\Message\EmailMessage;
+use App\Repository\EmailRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -14,16 +16,25 @@ class NotificationCommand extends Command
 
     /** @var MessageBusInterface **/
     private MessageBusInterface $bus;
+    /** @var EmailRepository **/
+    private EmailRepository $repository;
 
-    public function __construct(MessageBusInterface $bus)
+    public function __construct(MessageBusInterface $bus, EmailRepository $repository)
     {
         $this->bus = $bus;
+        $this->repository = $repository;
         parent::__construct();
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->bus->dispatch(new EmailMessage('test'));
+        $emails = $this->repository->findAll();
+        $output->writeln('Получен список email для рассылки');
+
+        foreach ($emails as $email) {
+            $output->writeln('Отправляется сообщение для ' . $email->getAddress());
+            $this->bus->dispatch(new EmailMessage('test', 'test'));
+        }
 
         return Command::SUCCESS;
     }
--- src/Message/EmailMessage.php
+++ src/Message/EmailMessage.php
@@ -4,13 +4,14 @@ namespace App\Message;
 
 class EmailMessage
 {
-    /**
-     * @var string
-     */
+    /** @var string */
+    private $address;
+    /** @var string */
     private $message;
 
-    public function __construct(string $message)
+    public function __construct(string $address, string $message)
     {
+        $this->address = $address;
         $this->message = $message;
     }
 
@@ -20,5 +21,13 @@ class EmailMessage
     public function getMessage(): string
     {
         return $this->message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress(): string
+    {
+        return $this->address;
     }
 }
