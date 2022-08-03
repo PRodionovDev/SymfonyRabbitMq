783967a6aefd18fab1afa41b8d49ccb9ecc63f49
Rodionov Pavel <pavel.rodionov.developer@gmail.com>
2022-08-04 07:25:41 +0800

    удалены лишние методы из репозитория

--- src/Repository/EmailRepository.php
+++ src/Repository/EmailRepository.php
@@ -20,47 +20,4 @@ class EmailRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Email::class);
     }
-
-    public function add(Email $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(Email $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-//    /**
-//     * @return Email[] Returns an array of Email objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('e.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Email
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
